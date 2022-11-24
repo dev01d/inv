08067a95b163c429538474d52cdda177d305ec4e
Jason Behnke <jason@jasonbehnke.io>
2022-11-24 15:27:11 -0800

    Add nmap

---
+++ cmd/nmap/nmap.go
@@ -0,0 +1,42 @@
+package nmap
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Ullaakut/nmap/v2"
+	"github.com/fatih/color"
+)
+
+func Nmap(nmapVar string) {
+	scanner, err := nmap.NewScanner(
+		nmap.WithTargets(nmapVar),
+		nmap.WithFastMode(),
+	)
+	if err != nil {
+		log.Fatalf("unable to create nmap scanner: %v", err)
+	}
+
+	result, warnings, err := scanner.Run()
+
+	if err != nil {
+		log.Fatalf("unable to run nmap scan: %v", err)
+	}
+
+	if warnings != nil {
+		log.Printf("Warnings: \n %v", warnings)
+	}
+
+	for _, host := range result.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
+		}
+
+		fmt.Printf("Host")
+		color.Blue(" %q", host.Addresses[0])
+
+		for _, port := range host.Ports {
+			fmt.Printf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
+		}
+	}
+}
